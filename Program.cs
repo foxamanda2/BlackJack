543cf11fc86b88aaa95da5d330c56572901cfe10
Amanda Fox <fox.amanda2@gmail.com>
2021-01-11 21:55:10 -0500

    Working Code

--- Program.cs
+++ Program.cs
@@ -1,282 +1,277 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace BlackJack
+namespace Notes_Week_2
 {
-    public class Card
+    class Card
     {
         // Properties of a Card
         public string Suit { get; set; }
         public string Face { get; set; }
-
-        public List<string> DeckOfCards { get; set;}=new List<string>();
-        // public int Value { get; set; }
-
+        public int Value { get; set; }
         // Method for how a card should look
         public string CardBuild()
         {
             return ($"{Face} of {Suit}.");
-
         }
-        public int Value()
-        {
-            if (Face == "Ace")
-            {
-                return 11;
-            }
-            if (Face == "King" || Face == "Queen" || Face == "Jack" || Face == "10")
-            {
-                return 10;
-            }
-            if (Face == "2" || Face == "3" || Face == "4" || Face == "5" || Face == "6" || Face == "7" || Face == "8" || Face == "9")
-            {
-                return int.Parse(Face);
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        
+    }
+    class Deck
+    {
+        // List properties for Deck (Deck now is where Card is stored)
+        public List<Card> CardsInDeck { get; set; } = new List<Card>();
+        public List<Card> PlayerHand { get; set; } = new List<Card>();
+        public List<Card> HouseHand { get; set; } = new List<Card>();
+        //  Method to add a Deck to a list in class Cards
         public void CreateDeck()
         {
             var suitList = new List<string>() { "Spades", "Diamonds", "Hearts", "Clubs" };
             var faceList = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-
-
             foreach (var suit in suitList)
             {
                 foreach (var face in faceList)
                 {
-                 
-                    Face = face;
-                    Suit = suit;
-                    DeckOfCards.Add($"The {Face} of {Suit}");
-                    
-                }
+                    var newCard = new Card();
+                    newCard.Suit = suit;
+                    newCard.Face = face;
+                    int value = 0;
 
+                    if (face == "Ace")
+                    {
+                        value = 11;
+                    }
+                    if (face == "King" || face == "Queen" || face == "Jack" || face == "10")
+                    {
+                        value = 10;
+                    }
+                    if (face == "2" || face == "3" || face == "4" || face == "5" || face == "6" || face == "7" || face == "8" || face == "9")
+                    {
+                        value = int.Parse(face);
+                    }
+                    newCard.Value = value;
+                    CardsInDeck.Add(newCard);
+                }
             }
         }
-    
-
+        //  Method to shuffle Cards
         public void Shuffle()
         {
-            var numOfCards = DeckOfCards.Count;
+            var numOfCards = CardsInDeck.Count;
             for (var rightindex = numOfCards - 1; rightindex >= 0; rightindex--)
             {
                 var randomNumberGenerator = new Random();
                 var leftindex = randomNumberGenerator.Next(rightindex);
-                var leftNum = DeckOfCards[leftindex];
-                var rightNum = DeckOfCards[rightindex];
-             DeckOfCards[rightindex] = leftNum;
-             DeckOfCards[leftindex] = rightNum;
-
+                var leftNum = CardsInDeck[leftindex];
+                var rightNum = CardsInDeck[rightindex];
+                CardsInDeck[rightindex] = leftNum;
+                CardsInDeck[leftindex] = rightNum;
             }
-
         }
-         public void PrintAllCards()
+        //  Method to print all of the cards
+        public void PrintCards()
         {
-            foreach (var cardToPrint in DeckOfCards)
+            foreach (var cardToPrint in CardsInDeck)
             {
-                Console.WriteLine(cardToPrint);
+                Console.WriteLine(cardToPrint.CardBuild());
             }
         }
-
-
-    }
-    public class Hand:Card
-    {
-        public List<Card> HandofCards { get; set; } = new List<Card>();
-
-        public void CardHand(Card cardToPlaceInHand)
+        //  Method incase extra player cards are added to the instances
+        public void PlayerCard()
         {
-            HandofCards.Add(cardToPlaceInHand);
+            var firstCard = CardsInDeck[0];
+            CardsInDeck.Remove(firstCard);
+            PlayerHand.Add(firstCard);
         }
-
-        public int TotalValue()
+        // Method incase extra house cards are added to the instances
+        public void HouseCard()
         {
-            var HandTotal = 0;
-            foreach (var CardValueToPrint in HandofCards)
+            var firstCard = CardsInDeck[0];
+            CardsInDeck.Remove(firstCard);
+            HouseHand.Add(firstCard);
+        }
+        //  Method to print Player cards
+        public void PrintPlayerCards()
+        {
+            var playertotal = 0;
+            foreach (var cardToPrint in PlayerHand)
             {
-                var CurrentCardValue = CardValueToPrint.Value();
-                HandTotal = HandTotal + CurrentCardValue;
+                playertotal = playertotal + cardToPrint.Value;
+                Console.WriteLine(cardToPrint.CardBuild());
             }
-            return HandTotal;
-
+            Console.WriteLine($"Your total is {playertotal}");
         }
-        public void CardsInHand()
+        //  Method to print House cards
+        public void PrintHouseCards()
         {
-            foreach (var card in HandofCards)
+            var Housetotal = 0;
+            foreach (var cardToPrint in HouseHand)
             {
-                Console.WriteLine($"The {card.Suit} of {card.Face}");
+                Housetotal = Housetotal + cardToPrint.Value;
+                Console.WriteLine(cardToPrint.CardBuild());
             }
-            var TotalValue=TotalValue();
-            Console.WriteLine($"Total value is: {TotalValue}");
         }
-    }    
-        // public void HitStand(string prompt)
-        //  {
-        //     //  Prompt for Hit or Stand
-        //     Console.Write(prompt);
-        //     Console.WriteLine("\n");
-        //     var HorS = Console.ReadLine().ToLower();
-
-        //     // Initializing counter for total score
-        //     var playertotal = 0;
-
-        //     // If player Decides to Hit program will add card (Only adds one card)
-        //     if (HorS == "h" || HorS == "hit")
-        //     {
-        //     var firstCard = DeckOfCards[0];
-        //      DeckOfCards.Remove(firstCard);
-        //     HandofCards.Add(firstCard);
-
-        //     //  WriteLine Cards in hand and total points
-        //     foreach (var cardToPrint in HandofCards)
-        //     {
-        //     playertotal = playertotal + cardToPrint.Value;
-        //     Console.WriteLine($"Your hand is: {cardToPrint.CardBuild()}");
-
-        //     }
-        //     Console.WriteLine("\n");
-        //     Console.Write($"Your Total is: {playertotal}");
-        //     Console.WriteLine("\n");
-
-        //         // If Player gets over 21 Automatically bust and close game
-        //         if (playertotal > 21)
-        //         {
-        //             Console.WriteLine("\n");
-        //             Console.WriteLine("You busted and lost");
-        //             Environment.Exit(-1);
-        //         }
-        //     }
-
-        //     //  WriteLine cards in list if stand is selected
-        //     if (HorS == "s" || HorS == "stand")
-        //     {
-        //         foreach (var cardToPrint in HandofCards)
-        //         {
-        //             playertotal = playertotal + cardToPrint.Value;
-        //             Console.WriteLine($"Your hand is: {cardToPrint.CardBuild()}");
-        //         }
-        //         Console.WriteLine("\n");
-        //         Console.Write($"Your total is {playertotal}");
-        //     }
-
-
-        // }
-        // public void Houseplay()
-        // {
-        //     var HouseFinal = 0;
-
-        //     //  Adding Player Final Number
-        //     var PlayerFinal = 0;
-        //     foreach (var cardToPrint in HandofCards)
-        //     {
-        //         PlayerFinal = PlayerFinal + cardToPrint.Value;
-        //     }
-
-        //     //  Hitting for House
-        //     if (HouseFinal < 17)
-        //     {
-        //         var firstCard = DeckOfCards[0];
-        //      DeckOfCards.Remove(firstCard);
-        //         HouseHand.Add(firstCard);
-        //     }
-
-        //     //  Finding House total and Cards
-        //     foreach (var cardToPrint in HouseHand)
-        //     {
-        //         HouseFinal = HouseFinal + cardToPrint.Value;
-        //         Console.WriteLine($"House hand is:{cardToPrint.CardBuild()}");
-        //     }
-        //     Console.WriteLine("\n");
-        //     Console.WriteLine($"Final House Total:{HouseFinal}");
-
-
-        //     //  Comparing House total to Player Total
-        //     if (HouseFinal > 21 && PlayerFinal == 21)
-        //     {
-        //         Console.WriteLine("\n");
-        //         Console.WriteLine("You Won.");
-        //         Console.WriteLine("\n");
-        //     }
-        //     if (HouseFinal > 21)
-        //     {
-        //         Console.WriteLine("\n");
-        //         Console.WriteLine("House Busted. You Won.");
-        //         Console.WriteLine("\n");
-        //         Environment.Exit(-1);
-        //     }
-
-        //     if (HouseFinal > PlayerFinal)
-        //     {
-        //         Console.WriteLine("\n");
-        //         Console.WriteLine("House won. I am sorry you lost");
-        //         Console.WriteLine("\n");
-        //     }
-        //     if (HouseFinal < PlayerFinal)
-        //     {
-        //         Console.WriteLine("\n");
-        //         Console.WriteLine("YOU WON!");
-        //         Console.WriteLine("\n");
-        //     }
-
-        //     if (HouseFinal == PlayerFinal)
-        //     {
-        //         Console.WriteLine("\n");
-        //         Console.WriteLine("House won. I am sorry you lost");
-        //         Console.WriteLine("\n");
-        //     }
-
-
-    public class Program
-    {
-        // static void Displaygame()
-        // {
-        //     Console.WriteLine();
-        //     Console.WriteLine("Welcome to BlackJack: A Game of Chances and Struggle");
-        //     Console.WriteLine();
-        // }
-
-        //  //  Prompt for playing game
-        // static void BeginPrompt(string prompt)
-        // {
-        //     Console.Write(prompt);
-        //     var openingQuestion = Console.ReadLine().ToLower();
-        //     if (openingQuestion == "yes" || openingQuestion == "y")
-        //     {
-        //         Console.WriteLine("Let the game begin");
-        //     }
-        //     else
-        //     {
-        //         Console.WriteLine("Okay no game today.");
-        //         Environment.Exit(-1);
-        //     }
-        // }
+        // Prompt for Overall Game
+        public void HitStand(string prompt)
+        {
+            //  Prompt for Hit or Stand
+            Console.Write(prompt);
+            Console.WriteLine("\n");
+            var HorS = Console.ReadLine().ToLower();
+            // Initializing counter for total score
+            var playertotal = 0;
+            // If player Decides to Hit program will add card (Only adds one card)
+            if (HorS == "h" || HorS == "hit")
+            {
+                var firstCard = CardsInDeck[0];
+                CardsInDeck.Remove(firstCard);
+                PlayerHand.Add(firstCard);
+                //  WriteLine Cards in hand and total points
+                foreach (var cardToPrint in PlayerHand)
+                {
+                    playertotal = playertotal + cardToPrint.Value;
+                    Console.WriteLine($"Your hand is: {cardToPrint.CardBuild()}");
+                }
+                Console.WriteLine("\n");
+                Console.Write($"Your Total is: {playertotal}");
+                Console.WriteLine("\n");
+                // If Player gets over 21 Automatically bust and close game
+                if (playertotal > 21)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("You busted and lost");
+                    Environment.Exit(-1);
+                }
+            }
+            //  WriteLine cards in list if stand is selected
+            if (HorS == "s" || HorS == "stand")
+            {
+                foreach (var cardToPrint in PlayerHand)
+                {
+                    playertotal = playertotal + cardToPrint.Value;
+                    Console.WriteLine($"Your hand is: {cardToPrint.CardBuild()}");
+                }
+                Console.WriteLine("\n");
+                Console.Write($"Your total is {playertotal}");
+            }
+        }
+        public void Houseplay()
+        {
+            var HouseFinal = 0;
+            //  Adding Player Final Number
+            var PlayerFinal = 0;
+            foreach (var cardToPrint in PlayerHand)
+            {
+                PlayerFinal = PlayerFinal + cardToPrint.Value;
+            }
+            //  Hitting for House
+            if (HouseFinal < 17)
+            {
+                var firstCard = CardsInDeck[0];
+                CardsInDeck.Remove(firstCard);
+                HouseHand.Add(firstCard);
+            }
+            //  Finding House total and Cards
+            foreach (var cardToPrint in HouseHand)
+            {
+                HouseFinal = HouseFinal + cardToPrint.Value;
+                Console.WriteLine($"House hand is:{cardToPrint.CardBuild()}");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine($"Final House Total:{HouseFinal}");
+            //  Comparing House total to Player Total
+            if (HouseFinal > 21 && PlayerFinal == 21)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("You Won.");
+                Console.WriteLine("\n");
+            }
+            if (HouseFinal > 21)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("House Busted. You Won.");
+                Console.WriteLine("\n");
+                Environment.Exit(-1);
+            }
+            if (HouseFinal > PlayerFinal)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("House won. I am sorry you lost");
+                Console.WriteLine("\n");
+            }
+            if (HouseFinal < PlayerFinal)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("YOU WON!");
+                Console.WriteLine("\n");
+            }
+            if (HouseFinal == PlayerFinal)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("House won. I am sorry you lost");
+                Console.WriteLine("\n");
+            }
+        }
+        static void Displaygame()
+            {
+                Console.WriteLine();
+                Console.WriteLine("Welcome to BlackJack: A Game of Chances, Struggle, and Tears");
+                Console.WriteLine();
+            }
+        //  Prompt for playing game
+        static void BeginPrompt(string prompt)
+        {
+            Console.Write(prompt);
+            var openingQuestion = Console.ReadLine().ToLower();
+            if (openingQuestion == "yes" || openingQuestion == "y")
+            {
+                Console.WriteLine("Let the game begin");
+            }
+            else
+            {
+                Console.WriteLine("Okay no game today.");
+                Environment.Exit(-1);
+            }
+        }
         static void Main(string[] args)
         {
-                // Opening Prompts
-                Displaygame();
-                BeginPrompt("Are you ready to play? Yes or No? ");
+            // Opening Prompts
+            Displaygame();
+            BeginPrompt("Are you ready to play? Yes or No? ");
 
+            var game1 = new Deck();
+            game1.CreateDeck();
+            game1.Shuffle();
+            game1.PlayerCard();
+            game1.PlayerCard();
+            game1.PrintPlayerCards();
+            Console.WriteLine("------------");
+            game1.HitStand("Do you want to hit or stand?");
+            Console.WriteLine("\n");
+            game1.HouseCard();
+            game1.HouseCard();
+            game1.Houseplay();
 
-                // New Game
-                // var game1 = new Card();
-                // game1.CreateDeck();
-                // game1.Shuffle();
-                // game1.CardsInHand(); 
-
-                var firstPlayerCard = deck[0];
-                DeckOfCards.Remove(firstPlayerCard); 
-
-                
-                var playerHand = new Hand();
-                playerHand.CreateDeck();
-                playerHand.Shuffle();
-                playerHand.CardsInHand(); 
-                playerHand.CardsInHand(firstPlayerCard); 
+            // Play 1 more time
+            Console.Write("\n\nThat was fun. Play again? YES or NO ");
+            var keepPlaying = Console.ReadLine().ToLower();
+            while(keepPlaying=="y"||keepPlaying=="yes")
+            {
+                var game2 = new Deck();
+                game2.CreateDeck();
+                game2.Shuffle();
+                game2.PlayerCard();
+                game2.PlayerCard();
+                game2.PrintPlayerCards();
+                Console.WriteLine("------------");
+                game2.HitStand("Do you want to hit or stand?");
+                Console.WriteLine("\n");
+                game2.HouseCard();
+                game2.HouseCard();
+                game2.Houseplay();
+            
+            }
 
         }
-    }}
+    }
+}
+
         
